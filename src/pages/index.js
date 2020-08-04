5d6a11c63f72fadb75f38eb66879d7d4c51d676a
Ryan <rdturner40@gmail.com>
2020-08-04 03:20:10 -0400

    added graphcms source, updated hero styles. Trying to stick with styled components styling methods

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import Hero from '../components/Hero/Hero'
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <p style={{marginTop: 60}}> Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm.Pinnace holystone mizzenmast quarter crow 's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters.
-
-    Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors.Bring a spring upon her cable holystone blow the man down spanker Shiver me timbers to go on account lookout wherry doubloon chase.Belay yo - ho - ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to. </p>
+    <Hero/>
     <Link to="/playground/">Playground</Link> <br />
   </Layout>
 )
